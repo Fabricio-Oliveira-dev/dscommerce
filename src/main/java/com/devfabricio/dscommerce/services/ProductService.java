44ca44eddc815181be376dbb74f8816dc2e1d157
Fabricio Pita <fabricio001400@gmail.com>
2023-05-13 15:58:54 -0300

    get products paged

--- src/main/java/com/devfabricio/dscommerce/services/ProductService.java
+++ src/main/java/com/devfabricio/dscommerce/services/ProductService.java
@@ -4,8 +4,12 @@ import com.devfabricio.dscommerce.dto.ProductDTO;
 import com.devfabricio.dscommerce.entities.Product;
 import com.devfabricio.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -17,5 +21,11 @@ public class ProductService {
     public ProductDTO findById(Long id) {
         Product product = repository.findById(id).get();
         return new ProductDTO(product);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 }
