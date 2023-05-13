5ed2a7d18772355f5d50173bb4739dea50adb34c
Fabricio Pita <fabricio001400@gmail.com>
2023-05-13 19:06:17 -0300

    Exception handling

---
+++ src/main/java/com/devfabricio/dscommerce/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.devfabricio.dscommerce.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
