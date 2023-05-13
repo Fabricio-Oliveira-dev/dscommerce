a23c91f689745e01a4a1454798acff22ed08b254
Fabricio Pita <fabricio001400@gmail.com>
2023-05-13 20:02:05 -0300

    Bean validation

--- src/main/java/com/devfabricio/dscommerce/dto/ProductDTO.java
+++ src/main/java/com/devfabricio/dscommerce/dto/ProductDTO.java
@@ -1,12 +1,23 @@
 package com.devfabricio.dscommerce.dto;
 
 import com.devfabricio.dscommerce.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 public class ProductDTO {
 
     private Long id;
+
+    @Size(min = 3, max = 80, message = "Nome precisa de ter entre 3 e 80 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String name;
+
+    @Size(min = 10, message = "Descrição precisa ter no mínimo 10 caracteres")
+    @NotBlank(message = "Campo requerido")
     private String description;
+
+    @Positive(message = "O preço deve ser positivo")
     private Double price;
     private String imgUrl;
 
