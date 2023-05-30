5a6f85cf081337504024267cdf2ac7acb8ab06ed
Fabricio Pita <fabricio001400@gmail.com>
2023-05-30 16:43:14 -0300

    userDetailsService

---
+++ src/main/java/com/devfabricio/dscommerce/services/UserService.java
@@ -0,0 +1,38 @@
+package com.devfabricio.dscommerce.services;
+
+import com.devfabricio.dscommerce.entities.Role;
+import com.devfabricio.dscommerce.entities.User;
+import com.devfabricio.dscommerce.projections.UserDetailsProjection;
+import com.devfabricio.dscommerce.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+        if (result.size() == 0) {
+            throw new UsernameNotFoundException("Email not found");
+        }
+
+        User user = new User();
+        user.setEmail(result.get(0).getUsername());
+        user.setPassword(result.get(0).getPassword());
+        for (UserDetailsProjection projection: result) {
+            user.addRole(new Role(projection.getRoleId(), projection.getAuthority()));
+        }
+
+        return user;
+    }
+}
