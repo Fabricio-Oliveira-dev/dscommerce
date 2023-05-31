b2614063dfa713a16cd72644e3f2e4a837cef86e
Fabricio Pita <fabricio001400@gmail.com>
2023-05-31 13:37:19 -0300

    ProductDTO with categories

---
+++ src/main/java/com/devfabricio/dscommerce/dto/CategoryDTO.java
@@ -0,0 +1,27 @@
+package com.devfabricio.dscommerce.dto;
+
+import com.devfabricio.dscommerce.entities.Category;
+
+public class CategoryDTO {
+
+    private Long id;
+    private String name;
+
+    public CategoryDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public CategoryDTO(Category entity) {
+        id = entity.getId();
+        name = entity.getName();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- src/main/java/com/devfabricio/dscommerce/services/ProductService.java
+++ src/main/java/com/devfabricio/dscommerce/services/ProductService.java
@@ -1,7 +1,9 @@
 package com.devfabricio.dscommerce.services;
 
+import com.devfabricio.dscommerce.dto.CategoryDTO;
 import com.devfabricio.dscommerce.dto.ProductDTO;
 import com.devfabricio.dscommerce.dto.ProductMinDTO;
+import com.devfabricio.dscommerce.entities.Category;
 import com.devfabricio.dscommerce.entities.Product;
 import com.devfabricio.dscommerce.repositories.ProductRepository;
 import com.devfabricio.dscommerce.services.exceptions.DatabaseException;
@@ -74,5 +76,12 @@ public class ProductService {
         entity.setDescription(dto.getDescription());
         entity.setPrice(dto.getPrice());
         entity.setImgUrl(dto.getImgUrl());
+
+        entity.getCategories().clear();
+        for (CategoryDTO catDTO: dto.getCategories()) {
+            Category cat = new Category();
+            cat.setId(catDTO.getId());
+            entity.getCategories().add(cat);
+        }
     }
 }
